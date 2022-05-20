e6975cdae00fda62b8cddd009abdbc55590f801d
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-05-20 03:18:42 +0300

    inprocess

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import (
+                            TemplateView,
+                            ListView,
+                            DeleteView,
+                            DetailView,
+                            CreateView,
+                            UpdateView
+                        )
 
 # Create your views here.
+
+class HomeView (TemplateView):
+    template_name = "home.html"
+
+
+
